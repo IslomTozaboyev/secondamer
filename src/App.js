f3352fa649c5c86e7015c8cd4c9269f66385e359
IslomTozaboyev <itozaboyev@gmail.com>
2021-07-23 14:47:57 +0500

    second

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
   return (
     <div className="text-center">
       <div className="clock">
-         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
+         <span>{("0" + Math.floor((time / 600000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 60000) % 60)).slice(-2)}:</span>
         <span>{("0" + Math.floor((time / 1000) % 60)).slice(-2)}:</span>
         <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
